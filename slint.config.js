871187f0d2005bba816c9f375de103223bfc84f0
Achmad Aulia Irsyad <achmadauliairsyad@gmail.com>
2025-11-22 22:20:50 +0800

    add lint config

---
+++ slint.config.js
@@ -0,0 +1,14 @@
+// eslint.config.js
+export default [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "module",
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-console": "off",
+    },
+  },
+];
