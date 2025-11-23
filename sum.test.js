4a807b5a33f1f123a158a76e390c0d45eb2f705a
Achmad Aulia Irsyad <achmadauliairsyad@gmail.com>
2025-11-23 20:37:59 +0800

    test unit

--- sum.test.js
+++ sum.test.js
@@ -1,6 +1,11 @@
-// sum.test.js
 const { sum } = require("./sum");
 
-test("menjumlahkan dua angka", () => {
-  expect(sum(2, 3)).toBe(5);
+test("menjumlahkan dua angka dengan benar", () => {
+  const result = sum(2, 3);
+  expect(result).toBe(5);
+});
+
+test("hasil penjumlahan angka negatif", () => {
+  const result = sum(-1, 4);
+  expect(result).toBe(3);
 });
